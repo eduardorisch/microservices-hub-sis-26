7dbec121f7a71824e6a5bb4ce71bf0ffd449f6be
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 21:58:43 -0300

    feat ms-pagamento: first commit

---
+++ ms-pagamentos/src/test/java/com/github/eduardorisch/ms/pagamentos/MsPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eduardorisch.ms.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
